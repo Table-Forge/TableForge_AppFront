f24aa51f790700e764cc7351d66934a733128e1b
ferrazmanu <ferraz.manuela@hotmail.com>
2026-07-01 19:58:04 -0300

    fix(navigation): update deprecated navigate signature in tab-bar

--- src/components/tab-bar/tab-bar.tsx
+++ src/components/tab-bar/tab-bar.tsx
@@ -72,11 +72,7 @@ export const TabBar = ({
             });
 
             if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate({
-                name: route.name,
-                merge: true,
-                params: route.params,
-              });
+              navigation.navigate(route.name, route.params);
             }
           };
 
